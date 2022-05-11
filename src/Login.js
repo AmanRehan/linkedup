0ce44a1aa380d26163eda3ceb2e3a2391f273541
Aman Rehan <amanmanzerrehan@gmail.com>
2022-05-11 23:44:42 +0530

    commit

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,41 @@
+import React, {useState} from 'react';
+import { auth } from "./firebase";
 import './Login.css'
 
 function Login() {
 
-    const register = () =>{};
-    const loginToApp = () =>{};
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+
+    const register = () =>{
+        if(!name){
+            return alert("Please enter a full name.")
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).then(userAuth => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                profileURL: profilePic,
+            })
+        })
+    };
+    const loginToApp = (e) =>{
+        e.preventDefault();
+    };
   return (
     <div className="login">
-      {/* <h1>Login Component </h1> */}
       <img src="https://1000logos.net/wp-content/uploads/2017/03/Font-of-the-LinkedIn-Logo.jpg" alt="" />
 
 
       <form action="">
-        <input placeholder="Full Name (if registering)" type="text" />
+        <input value={name} onChange={e => setName(e.target.value)} placeholder="Full Name (if registered)" type="text" />
 
-        <input placeholder="Profile Pic URL (optional)" type="text" />
+        <input value={profilePic} onClick={e => setProfilePic(e.target.value)} placeholder="Profile Pic URL (optional)" type="text" />
 
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="text" />
+        <input value={email} onClick={e => setEmail(e.target.value)} placeholder="Email" type="email" />
+        <input value={password} onClick={e => setPassword(e.target.value)} placeholder="Password" type="text" />
         <button type="submit" onClick={loginToApp}>Sign In</button>
       </form>
       <p>Not a member.
