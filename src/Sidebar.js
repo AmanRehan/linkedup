8c26d106a48f76be442704a3bd839a67ab13c301
Aman Rehan <amanmanzerrehan@gmail.com>
2022-05-14 20:06:36 +0530

    Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,50 +1,52 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
-import './Sidebar.css'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import "./Sidebar.css";
 
 function Sidebar() {
+  const { user } = useSelector(selectUser);
 
-    const recentItem = (topic) => (
-        <div className="sidebar__recentItem">
-            <span className="sidebar__hash">#</span>
-            <p>{topic}</p>
-        </div>
-    );
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
 
   return (
     <div className="sidebar">
-        <div className="sidebar__top">
-            <img src="https://images.pexels.com/photos/7135121/pexels-photo-7135121.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
-            <Avatar className="sidebar__avatar"/>
-            <h2>Aman Rehan</h2>
-            <h4>amanrehan@gmail.com</h4>
-        </div>
+      <div className="sidebar__top">
+        <img
+          src="https://images.pexels.com/photos/7135121/pexels-photo-7135121.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+          alt=""
+        />
+        <Avatar src={user.photoUrl} className="sidebar__avatar" />
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
+      </div>
 
-        <div className="sidebar__stats">
-            <div className="sidebar__stat">
-                <p>Who viewed you</p>
-                <p className="sidebar__statNumber">
-                    2,567
-                </p>
-            </div>
-            <div className="sidebar__stat">
-                <p>Views on post</p>
-                <p className="sidebar__statNumber">
-                    2,898
-                </p>
-            </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,567</p>
         </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,898</p>
+        </div>
+      </div>
 
-        <div className="sidebar__bottom">
-            <p>Recent</p>
-            {recentItem("reactjs")}
-            {recentItem("programming")}
-            {recentItem("developer")}
-            {recentItem("design")}
-            {recentItem("engineer")}
-        </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("developer")}
+        {recentItem("design")}
+        {recentItem("engineer")}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
