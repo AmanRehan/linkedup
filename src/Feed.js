58b0bae8f38ec3613a8e010ffc82d82ded1c8630
Ankur Paul <suryashi2013@gmail.com>
2022-05-18 06:04:37 +0000

    Fixed an issue for recent posts index on feed
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

--- src/Feed.js
+++ src/Feed.js
@@ -3,7 +3,7 @@ import CreateIcon from "@mui/icons-material/Create";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import ImageIcon from "@mui/icons-material/Image";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
-import { addDoc, collection, onSnapshot, orderBy, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, orderBy, serverTimestamp, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 import "./Feed.css";
@@ -12,85 +12,92 @@ import InputOption from "./InputOption";
 import Post from "./Post";
 
 /* 
-	Firebase v9 (Modular docs)
-	https://firebase.google.com/docs/firestore/manage-data/add-data#add_a_document
+  Firebase v9 (Modular docs)
+  https://firebase.google.com/docs/firestore/manage-data/add-data#add_a_document
 */
 
 function Feed() {
-	const [input, setInput] = useState("");
-	const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
 
-	const fetchPosts = () => {
-		// Firestore collection, onSnapshot v9 (Use modular approach, do not use namespaced)
-		// To use namespaced imports, use firebase sdk v9
-		onSnapshot(collection(db, "posts"),orderBy("timestamp", "desc"), (snapshot) =>
-			setPosts(
-				snapshot.docs.map((doc) => ({
-					id: doc.id,
-					data: doc.data(),
-				}))
-			)
-		);
+  const fetchPosts = async () => {
+    // Get the documents from firestore under the posts collection
+    const querySnapshot = await getDocs(collection(db, "posts"), orderBy("timeStamp", "desc"));
+    // Sort the querySnapshot by timeStamp
+    const sortedPosts = querySnapshot.docs.sort((a, b) => {
+      return b.data().timeStamp.seconds - a.data().timeStamp.seconds;
+    });
+    sortedPosts.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().message}`);
+    });
+
+    // Firestore collection, onSnapshot v9 (Use modular approach, do not use namespaced)
+    // To use namespaced imports, use firebase sdk v9
+    setPosts(
+      sortedPosts.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }))
+    );
     setInput("")
-	}
+  }
 
-	useEffect(() =>fetchPosts(), []);
+  useEffect(() => { fetchPosts() }, []);
 
-	const sendPost = async (e) => {
-		e.preventDefault();
+  const sendPost = async (e) => {
+    e.preventDefault();
 
-		const addObject = {
-			name: "Aman Rehan",
-			description: "This is a post",
-			message: input,
-			photoUrl: "",
-			timeStamp: serverTimestamp(),
-		}
-		// Add object to firestore (v9 Modular Syntax)
-		const docRef = await addDoc(collection(db, "posts"), addObject)
-		console.table(docRef)
-		if (docRef.id) {
-			fetchPosts();
-		}
+    const addObject = {
+      name: "Aman Rehan",
+      description: "This is a post",
+      message: input,
+      photoUrl: "",
+      timeStamp: serverTimestamp(),
+    }
+    // Add object to firestore (v9 Modular Syntax)
+    const docRef = await addDoc(collection(db, "posts"), addObject)
+    console.log(docRef)
+    if (docRef.id)
+      fetchPosts();
 
-		// Firebase SDK v8 syntax
-		// db.collection("posts").add({
-		// 	name: "Aman Rehan",
-		// 	description: "This is a post",
-		// 	message: input,
-		// 	photoUrl: "",
-		// 	timeStamp: firebase.fireStore.FieldValue.serverTimeStamp(),
-		// });
-	};
+    // Firebase SDK v8 syntax
+    // db.collection("posts").add({
+    // 	name: "Aman Rehan",
+    // 	description: "This is a post",
+    // 	message: input,
+    // 	photoUrl: "",
+    // 	timeStamp: firebase.fireStore.FieldValue.serverTimeStamp(),
+    // });
+  };
 
-    return (
+  return (
     <div className="feed">
-        <div className="feed__inputContainer">
-            <div className="feed__input">
-                <CreateIcon />
-                <form action="">
-                    <input value={input} onChange={e => setInput(e.target.value)} type="text" />
-                    <button onClick={sendPost} type="submit">Send</button>
-                </form>
-            </div>
-            <div className="feed__inputOptions">
-                <InputOption Icon={ImageIcon} title="Photo" color="blue"/>
-                <InputOption Icon={SubscriptionsIcon} title="Video" color="green"/>
-                <InputOption Icon={EventNoteIcon} title="Event" color="orange "/>
-                <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="gray"/>
-            </div>
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
+          </form>
         </div>
-        {/* Post */}
-        {posts.map(({id, data: {name, description, message, photoUrl } }) =>(
-            <Post 
-                key={id}
-                name={name}
-                description={description}
-                message={message}
-                photoUrl={photoUrl}
-            />
-        ))}
-        {/* <Post name="Aman Rehan" description="Test" message="Wow This worked"/> */}
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="blue" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="green" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="orange " />
+          <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="gray" />
+        </div>
+      </div>
+      {/* Post */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
+      {/* <Post name="Aman Rehan" description="Test" message="Wow This worked"/> */}
     </div>
   )
 }
