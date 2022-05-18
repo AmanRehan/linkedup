56cc12f233aa86e085a58c01c5c453a531461394
Ankur Paul <suryashi2013@gmail.com>
2022-05-18 06:30:36 +0000

    Final fix for posts render

--- src/Feed.js
+++ src/Feed.js
@@ -61,57 +61,56 @@ function Feed() {
     if (docRef.id) {
       fetchPosts();
     }
-
-    // Firebase SDK v8 syntax
-    // db.collection("posts").add({
-    // 	name: "Aman Rehan",
-    // 	description: "This is a post",
-    // 	message: input,
-    // 	photoUrl: "",
-    // 	timeStamp: firebase.fireStore.FieldValue.serverTimeStamp(),
-    // });
-
-    return (
-      <div className="feed">
-        <div className="feed__inputContainer">
-          <div className="feed__input">
-            <CreateIcon />
-            <form action="">
-              <input
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                type="text"
-              />
-              <button onClick={sendPost} type="submit">
-                Send
-              </button>
-            </form>
-          </div>
-          <div className="feed__inputOptions">
-            <InputOption Icon={ImageIcon} title="Photo" color="blue" />
-            <InputOption Icon={SubscriptionsIcon} title="Video" color="green" />
-            <InputOption Icon={EventNoteIcon} title="Event" color="orange " />
-            <InputOption
-              Icon={CalendarViewDayIcon}
-              title="Write Article"
-              color="gray"
-            />
-          </div>
-        </div>
-        {/* Post */}
-        {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
-          <Post
-            key={id}
-            name={name}
-            description={description}
-            message={message}
-            photoUrl={photoUrl}
-          />
-        ))}
-        {/* <Post name="Aman Rehan" description="Test" message="Wow This worked"/> */}
-      </div>
-    );
   }
+
+  // Firebase SDK v8 syntax
+  // db.collection("posts").add({
+  // 	name: "Aman Rehan",
+  // 	description: "This is a post",
+  // 	message: input,
+  // 	photoUrl: "",
+  // 	timeStamp: firebase.fireStore.FieldValue.serverTimeStamp(),
+  // });
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="blue" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="green" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="orange " />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="gray"
+          />
+        </div>
+      </div>
+      {/* Post */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
+      {/* <Post name="Aman Rehan" description="Test" message="Wow This worked"/> */}
+    </div>
+  );
 }
 
 export default Feed;
